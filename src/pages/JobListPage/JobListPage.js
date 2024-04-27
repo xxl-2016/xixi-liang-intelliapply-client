43f8625300c4721ca38d0fcd7fcd7230ba9831cf
Xixi Liang <lxx260905@gmail.com>
2024-04-27 15:39:42 -0700

    add JobListPage by using RapidAPI, fetch data successfully

--- src/pages/JobListPage/JobListPage.js
+++ src/pages/JobListPage/JobListPage.js
@@ -1,9 +1,41 @@
-import "./JobListPage.scss";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function JobListPage({ setIsUserLoggedIn }) {
+export default function JobSearch() {
+  const [keywords, setKeywords] = useState("");
+  const [jobList, setJobList] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get("http://localhost:6060/jobs", {
+        params: {
+          keywords: "developer",
+          locationId: "92000000",
+          datePosted: "anyTime",
+          sort: "mostRelevant",
+        },
+      });
+      setJobList(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="JobListPage">
-      <p>This is the Job List page</p>
+    <div>
+      <input
+        type="text"
+        value={keywords}
+        onChange={(e) => setKeywords(e.target.value)}
+        placeholder="Enter keywords..."
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <ul>
+        {jobList.map((job) => (
+          <li key={job.id}>{job.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
